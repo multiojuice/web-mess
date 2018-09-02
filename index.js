6e778e7888dcd9f25b167f840ade0f7b262eb46a
Owen J Sullivan <multiojuice@gmail.com>
2018-09-01 17:02:04 -0700

    Sending moving bullets, synced across all

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function onConnection(socket){
   socket.on('move', (data) => socket.broadcast.emit('move', data));
   socket.on('create', (data) => socket.broadcast.emit('create', data));
   socket.on('disconnect', (data) => socket.broadcast.emit('delete', socket.id));
+  socket.on('bullet', (data) => socket.broadcast.emit('bullet', data));
 }
 
 io.on('connect', onConnection);
--- public/main.js
+++ public/main.js
@@ -9,32 +9,25 @@
 
   document.addEventListener('keydown', (event) => {
   const keyName = event.key;
-  let px_val;
   switch (keyName) {
     case 's':
-      px_val = parseInt(icon.style.top || 0) + 1;
-      icon.style.top = `${px_val}%`;
-      break;
     case 'w':
-      px_val = parseInt(icon.style.top || 0) - 1;
-      icon.style.top = `${px_val}%`;
-      break;
     case 'd':
-      px_val = parseInt(icon.style.left || 0) + 1;
-      icon.style.left = `${px_val}%`;
-      break;
     case 'a':
-      px_val = parseInt(icon.style.left || 0) - 1;
-      icon.style.left = `${px_val}%`;
+      handleMove(keyName);
+      break;
+    case ' ':
+      socket.emit('bullet', {image: 'assets/enemy_bullet.svg', left: icon.style.left, top: icon.style.top})
+      onBullet({image: 'assets/friendly_bullet.svg', left: icon.style.left, top: icon.style.top});
       break;
   }
-  socket.emit('move', {id: socket.id, left: icon.style.left, top: icon.style.top})
 });
 
   socket.on('move', onMove);
   socket.on('create', onCreate);
   socket.on('connect', () => socket.emit('create', {id: socket.id}))
   socket.on('delete', onDelete);
+  socket.on('bullet', onBullet)
 
   function onMove(data) {
     let enemy = document.getElementById(data.id);
@@ -48,13 +41,54 @@
     const newIcon = document.createElement("img");
     newIcon.classList.add("enemy");
     newIcon.id = data.id;
-    newIcon.src = "tank.svg";
+    newIcon.src = "assets/enemy_plane.svg";
     wholeMap.appendChild(newIcon);
   }
 
   function onDelete(id) {
     const deletedPlayer = document.getElementById(id);
-    deletedPlayer.parentNode.removeChild(deletedPlayer);
-    console.log('deleted', id)
+    if(deletedPlayer) deletedPlayer.parentNode.removeChild(deletedPlayer);
+  }
+
+  function handleMove(keyName) {
+    let px_val;
+    switch (keyName) {
+      case 's':
+        px_val = parseInt(icon.style.top || 0) + 1;
+        icon.style.top = `${px_val}%`;
+        break;
+      case 'w':
+        px_val = parseInt(icon.style.top || 0) - 1;
+        icon.style.top = `${px_val}%`;
+        break;
+      case 'd':
+        px_val = parseInt(icon.style.left || 0) + 1;
+        icon.style.left = `${px_val}%`;
+        break;
+      case 'a':
+        px_val = parseInt(icon.style.left || 0) - 1;
+        icon.style.left = `${px_val}%`;
+        break;
+    }
+    socket.emit('move', {id: socket.id, left: icon.style.left, top: icon.style.top})
+  }
+
+  function onBullet(data) {
+    const newBullet = document.createElement("img");
+    newBullet.src = data.image;
+    newBullet.classList.add("bullet");
+    newBullet.style.top = data.top;
+    newBullet.style.left = data.left;
+    wholeMap.appendChild(newBullet);
+    let currentPos = parseInt(data.left);
+
+    let motionInterval = setInterval(function() {
+        currentPos += 1;
+        if (currentPos < 1 || currentPos >= 99) {
+          newBullet.parentNode.removeChild(newBullet);
+          clearInterval(motionInterval);
+        }
+        newBullet.style.left = currentPos+"%";
+    },20);
   }
 })();
